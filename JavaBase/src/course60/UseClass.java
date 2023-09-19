6bdde11a480929ea10bab81ddec0628d0c5f9a16
nimo <nimo@LAPTOP-LIO9673H>
2023-09-19 10:39:15 +0900

    ポリモーフィズムとオーバライドのプログラム #7

---
+++ JavaBase/src/course60/UseClass.java
@@ -0,0 +1,13 @@
+package course60;
+
+public class UseClass {
+	public static void main(String[] args) {
+		Animal dog = new Dog();
+		Animal cat = new Cat();
+
+		//ぽりもーふぃずむ
+		dog.nakigoe();
+		cat.nakigoe();
+
+	}
+}
